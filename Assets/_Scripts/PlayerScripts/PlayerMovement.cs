f48b271b4b5ac40bebd078e279cc2d5e26053e76
John Snevily <john.snevily@gmail.com>
2023-08-21 20:24:13 -0700

    Rotation
    Added player rotation.
    Cleaned up movement script a little

--- Assets/_Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float playerSpeed;
-    [SerializeField] float playerSpeedMax;
-    [SerializeField] float playerDashSpeed;
+    [SerializeField] float speed;
+    [SerializeField] float maxSpeedDivisor;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float rotationSpeed;
     float inputHorizontal;
     float inputVertical;
+    Vector2 movementInput;
     Rigidbody2D myRb;
 
     private void Start()
@@ -21,25 +23,43 @@ public class PlayerMovement : MonoBehaviour
     {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         inputVertical = Input.GetAxisRaw("Vertical");
+        movementInput = new Vector2(inputHorizontal, inputVertical);
     }
 
 
     private void FixedUpdate()
     {
+        setPlayerVelocity();
+        rotateTowardsInput();
+    }
+
+
+    private void setPlayerVelocity()
+    {
         if (inputHorizontal != 0 || inputVertical != 0)
         {
-            
+
             if (inputHorizontal != 0 && inputVertical != 0)
             {
-                inputHorizontal *= playerSpeedMax;
-                inputVertical *= playerSpeedMax;
+                inputHorizontal *= maxSpeedDivisor;
+                inputVertical *= maxSpeedDivisor;
             }
-            myRb.velocity = new Vector2(inputHorizontal * playerSpeed, inputVertical * playerSpeed);
+            myRb.velocity = new Vector2(inputHorizontal * speed, inputVertical * speed);
         }
 
         else
         {
-            myRb.velocity = new Vector2 (0f, 0f);
+            myRb.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    private void rotateTowardsInput()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, movementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+            myRb.MoveRotation(rotation);
         }
     }
 
