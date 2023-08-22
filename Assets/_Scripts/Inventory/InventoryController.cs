50d250e6e9e8d662ba3f2f57c35f7d248fded72a
Dannybossboy <danielsmirnoff8@gmail.com>
2023-08-21 20:58:14 -0700

    started on inventory controller

---
+++ Assets/_Scripts/Inventory/InventoryController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    //public List<Item>() = new List<Item>();\
+}
---
+++ Assets/_Scripts/Inventory/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string name;
+    public string description;
+    public Sprite icon;
+
+
+    public Item GetItemInstance() {
+        return Instantiate(this);
+    }
+
+}
