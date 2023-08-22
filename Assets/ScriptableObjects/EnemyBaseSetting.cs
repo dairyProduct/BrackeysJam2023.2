f0869572403085a4a43f54a857ac53549a633419
John Snevily <john.snevily@gmail.com>
2023-08-21 17:04:17 -0700

    Base Stat Attempt
    Set up some stats utilizing scriptableObjects

---
+++ Assets/ScriptableObjects/EnemyBaseSetting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyBaseSetting : StatBlock
+{
+
+    protected StatBlock entityStats;
+    public string EnemyTitle;
+    //More info?
+    //funcs to generate stats?
+
+}
---
+++ Assets/ScriptableObjects/StatBlock.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatBlock : ScriptableObject
+{
+    public int health;
+    public int stamina;
+    public int magic;
+}
