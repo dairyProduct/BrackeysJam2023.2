080537ad324f65cc3a2fe1a6a4b6e7520b5644b9
Dannybossboy <danielsmirnoff8@gmail.com>
2023-08-24 20:37:22 -0700

    changed animations and added dash fixed movement

--- Assets/_Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -7,11 +8,16 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float maxSpeedDivisor;
-    [SerializeField] float dashSpeed;
+    [SerializeField] float dashSpeed = 10f;
+    [SerializeField] float dashTime = .1f;
+    public float dashCD = .5f;
     [SerializeField] float rotationSpeed;
     Vector2 movement;
-    public Transform direction;
+    public Transform holder;
     Rigidbody2D myRb;
+    public TrailRenderer trailRenderer;
+
+    bool dashing;
 
     private void Start()
     {
@@ -23,15 +29,29 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(Dash());
+        }
     }
 
 
     private void FixedUpdate()
     {
+        if(dashing) return;
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         direction.Normalize();
-        myRb.velocity = (transform.up * movement.y + transform.right * movement.x) * speed;
-        //myRb.velocity = ((direction * movement.y) + (Vector2)(transform.right * movement.x)) * speed;
+        myRb.velocity = movement * speed;
+        //myRb.velocity = ((-holder.right * movement.y) + (holder.up * movement.x)) * speed;
+    }
+
+    IEnumerator Dash() {
+        dashing = true;
+        trailRenderer.enabled = true;
+        myRb.velocity = movement * dashSpeed;
+        yield return new WaitForSeconds(dashTime);
+        trailRenderer.enabled = false;
+        dashing = false;
     }
 
 
